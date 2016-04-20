63a6fb0d625a695642b6566d17005d9ca95b2461
Justin Holzmann <jphoenix@Justins-MacBook-Pro.local>
2016-04-20 15:44:47 -0500

    Adds make move logic that takes in a hash map as a move.

--- spec/tictactoe/board_spec.clj
+++ spec/tictactoe/board_spec.clj
@@ -4,11 +4,16 @@
 
 (describe "Display Board"
   (it "returns a blank board"
-      (let [temp-board { :a "a", :s "s", :d "d",
-                    :f "f", :g "g", :h "h",
-                    :j "j", :k "k", :l "l"}]
-        (should= temp-board display-board))))
+      (let [temp-board {:a "a" :s "s" :d "d"
+                        :f "f" :g "g" :h "h"
+                        :j "j" :k "k" :l "l"}]
+        (should= temp-board board))))
 
-;; :a "a", :s "s", :d "d",
-;; :f "f", :g "g", :h "h",
-;; :j "j", :k "k", :l "l"
+(describe "move"
+  (it "adds a move to a list of moves"
+      (let [temp-board {:a "x" :s "s" :d "d"
+                        :f "f" :g "g" :h "h"
+                        :j "j" :k "k" :l "l"},
+            move {:a "x"}]
+        (make-move move)
+        (should= temp-board board))))
