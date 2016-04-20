7d1203330b259727cea23166e80b2a13958f5d96
Justin Holzmann <jphoenix@Justins-MacBook-Pro.local>
2016-04-20 15:45:57 -0500

    Adds in logic for making a move from the board.

--- src/tictactoe/board.clj
+++ src/tictactoe/board.clj
@@ -1,6 +1,9 @@
 (ns tictactoe.board)
 
-(def display-board
-  {:a "a", :s "s", :d "d",
-  :f "f", :g "g", :h "h",
-  :j "j", :k "k", :l "l"})
+(def board {:a "a" :s "s" :d "d"
+            :f "f" :g "g" :h "h"
+            :j "j" :k "k" :l "l"})
+
+(defn make-move
+  [move]
+  (def board (conj board move)))
